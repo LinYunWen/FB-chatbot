b4efffbf6faadc814207f9eb446478f9c1b3d777
LinYunWen <l40303k@gmail.com>
2017-08-02 12:29:02 +0800

    text emojcon

--- server/__init__.py
+++ server/__init__.py
@@ -185,6 +185,8 @@ def handle_incoming_message():
     # request with not pure text message
     if "attachments" in messaging["message"]:
         print(data)
+        client.reply_text(sender_id, '❤️')
+        client.reply_text(sender_id, 'U+1F601')
         client.reply_image_url(sender_id,'https://scontent.xx.fbcdn.net/v/t39.1997-6/p100x100/851587_369239346556147_162929011_n.png?_nc_ad=z-m&oh=6c4deff5f1cf8c2f7940dab6f54862a2&oe=5A2DF5B0')
         response = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
         client.set_sender_action(sender_id, "typing_off")
