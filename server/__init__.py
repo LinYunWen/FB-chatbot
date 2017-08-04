9a0a6a0cb57a1be684869a75645614eca182517a
LinYunWen <l40303k@gmail.com>
2017-08-04 10:51:30 +0800

    fix urlparse module4

--- server/__init__.py
+++ server/__init__.py
@@ -16,8 +16,8 @@ from server.fbmsg import Fbmsg
 app = Flask(__name__)
 
 # connect to database
-urlparse.uses_netloc.append('postgres')
-url = urlparse.urlparse(os.environ['DATABASE_URL'])
+urllib.parse.uses_netloc.append('postgres')
+url = urllib.parse.urlparse(os.environ['DATABASE_URL'])
 conn = psycopg2.connect(
     database = url.path[1:],
     user = url.username,
